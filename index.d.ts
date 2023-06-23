cf8e47a34b952bd87b12191e02bbd58a18ac7a4b
manikandankan <70646002+manikandankan@users.noreply.github.com>
2023-06-23 11:02:57 +0530

    Add files via upload

---
+++ index.d.ts
@@ -0,0 +1,19 @@
+import * as react_jsx_runtime from 'react/jsx-runtime';
+
+interface HeaderProps {
+    /**
+     * The name of the application to display in the site header
+     */
+    applicationName: string;
+    /**
+     * The URL for the users avatar image
+     */
+    avatar?: string;
+    /**
+     * On click function for popover
+     */
+    onClickFunc?: () => void;
+}
+declare const Header: ({ applicationName, avatar, onClickFunc }: HeaderProps) => react_jsx_runtime.JSX.Element;
+
+export { Header };
